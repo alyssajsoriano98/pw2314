bbebacadbbe29b27a076e23e310dcb45a8018576
alyssajsoriano98 <130670575+alyssajsoriano98@users.noreply.github.com>
2023-05-15 18:22:55 +0200

    Add files via upload

--- esempio2.js
+++ esempio2.js
@@ -1,43 +1,113 @@
- /*modo 1
-let tr = document.createElement ("tr");
-let td = document.createElement ("td");
-td.innerHTML = "Aldo";
-tr.innerHTML(td);
-table.appendChild(tr);
+let nomi = ["Giacomo", "Aldo", "Giovanni", "Marina"];
+
+let prossimoNumero = 1;
+aggiungiNomiListaIniziale();
+
+function aggiungiNome(nome,numero){
+
+    
+    
+    let table = document.getElementById("tabella");
+    let tr = document.createElement("tr");
+    let tdNome = document.createElement("td");
+    tdNome.className = "nome";
+    tdNome.innerHTML = nome;
+    let tdNum = document.createElement("td");
+    tdNum.innerHTML = numero;
+    tr.appendChild(tdNum);
+    tr.appendChild(tdNome);
+    table.appendChild(tr);
+}
+
+function aggiungiNome(nome){
+
+    
+    
+    let table = document.getElementById("tabella");
+    let tr = document.createElement("tr");
+    let tdNome = document.createElement("td");
+    tdNome.className = "nome";
+    tdNome.innerHTML = nome;
+    let tdNum = document.createElement("td");
+    tdNum.innerHTML = prossimoNumero++;
+    tr.appendChild(tdNum);
+    tr.appendChild(tdNome);
+    table.appendChild(tr);
+
+   
+}
+
+function aggiungiNomiListaIniziale(){
+
+
+    
+    for(let i = 0; i < nomi.length; i++){
+      aggiungiNome(nomi[i]);
+    } 
+}
+
+function coloraRosso(){
+    let tdNomi = document.querySelectorAll(".nome");
+    for(tdNome of tdNomi){
+        tdNome.style.color="red";
+        //tdNome.classList.add("rosso");
+    }
+}
+
+// nella pagina precedente
+// inserire un input di testo e
+// un bottone aggiungi 
+// e aggiungere il nome 
+// alla tabella
+function aggiungiNomeNuovo(){
+    let nomeNuovo = document.getElementById('nomeNuovo').value;
+    // metodo 1: contare i tr 
+    /*let numeroNuovo = document.querySelectorAll("table#tabella tr");
+    console.log(numeroNuovo.length);
+
+    
+
+    // metodo 2: contare le righe della #tabella 
+    let tabella= document.getElementById("tabella");
+    console.log(tabella.rows.length);
+    
+    // metodo 3: contare dall'inizio i nomi
+    console.log(prossimoNumero);
+
+    // metodo 4: accedere all'ultimo elemento 
+    // della tabella (genericamente) e
+    // prendere il valore del td
+    let trList = document.querySelectorAll("table#tabella tr");
+    let myTd = trList[trList.length - 1].querySelectorAll("td")[0];
+    let numNuovo = parseInt(myTd.innerHTML)+1;
+    console.log(numNuovo);
+
+    //metodo 5: accedere all'ultimo elemento di una tabella
+    let tabella2= document.getElementById("tabella");
+    let numNuovo2 = parseInt(tabella2.rows[tabella2.rows.length - 1].cells[0].innerHTML) + 1;
+    console.log(numNuovo2);
 */
+    // utilizza il metodo 3
+    if ((nomeNuovo == "") || (nomeNuovo == "undefined")) {
+        //alert("Il campo Nome è obbligatorio.");
 
-/*
-for (let = i; i <= nomi.length; i++){
+        const toastLiveExample = document.getElementById('liveToast');
+
+
+        const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+        toastBootstrap.show();
+  
+
+        }
+
+        else {
+            aggiungiNome(nomeNuovo);
+            document.getElementById('nomeNuovo').value = "";
+        }
+    
+
+    // utilizza il metodo 1,2,4,5
+    //aggiungiNome(nomeNuovo,numNuovo);
 }
-*/ 
-
-let nomi = ["Aldo","Giovanni","Giacomo"]
-
-let table = document.getElementById("tabella")
-
-for(nome of nomi){
-
-let tr = document.createElement ("tr");
-let td = document.createElement ("td");
-td.innerHTML = nome;
-tr.appendChild(td);
-table.appendChild(tr);
-}
-
-//tabella con nomi e un numero progressivo
 
 
-let names = ["Bea","Ely","Lea"]
-
-let 
-
-
-
-
-
-
-
-//let p = document.getElementById("prova");
-//console.log(p)
-
-//p.innerHTML = "Sto modificando p"
