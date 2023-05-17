9dfbcdf6fe0adf0e7f1bc76429b99825d449ec0c
alyssajsoriano98 <130670575+alyssajsoriano98@users.noreply.github.com>
2023-05-17 17:37:33 +0200

    Add files via upload

--- indexbs.js
+++ indexbs.js
@@ -1,6 +1,39 @@
-document.querySelector('#nomi').style.background = "green";
-document.querySelector('#button').style.background = "pink";
+let nomi = ["Alyssa", "Gianni", "Giacomo", "Claudio", "Grey", "Filippo", "Beatrice", "Francesca","Andrea"];
 
-if (){
-    document.querySelector
+let nomiDaVisualizzare = [];
+for(nome of nomi){
+    nomiDaVisualizzare.push(nome);
 }
+
+mostraNomi();
+
+function mostraNomi(){
+    let contenitore = document.getElementById("contenitore-card");
+    contenitore.innerHTML = ""; 
+    //svuota contenitore perchè se no ogni volta che 
+    //facciamo appendchild lascia quelli iniziali
+
+    for(nome of nomiDaVisualizzare){
+        let h5 = document.createElement("h5");
+        h5.innerHTML=nome;
+        h5.className="card-title";
+        let card = document.createElement("div");
+        card.className = "card";
+        card.appendChild(h5);
+        contenitore.appendChild(card);
+    }
+
+}
+
+function filtro(){
+    let iniziale = document.getElementById("inizialeText").value;
+    nomiDaVisualizzare = [];
+    for(nome of nomi){
+        if(nome.toLowerCase().startsWith(iniziale.toLowerCase()))
+            nomiDaVisualizzare.push(nome);
+    }
+    mostraNomi();
+    
+
+}
+
