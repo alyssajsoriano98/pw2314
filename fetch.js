896ec63b85d928df9fc2ad7f648e75ed7cb96120
alyssajsoriano98 <130670575+alyssajsoriano98@users.noreply.github.com>
2023-05-22 01:41:26 +0200

    Add files via upload

--- fetch.js
+++ fetch.js
@@ -1,9 +1,93 @@
-let passaggi = [{"line":"56","hour":"17:01:20","realtime":true},{"line":"56","hour":"17:05:31","realtime":true},{"line":"56","hour":"17:19:50","realtime":true},{"line":"56","hour":"17:29:17","realtime":true},{"line":"56","hour":"17:41:57","realtime":true},{"line":"56","hour":"17:57:32","realtime":true},{"line":"56","hour":"18:05:39","realtime":true},{"line":"56","hour":"18:17:39","realtime":true},{"line":"56","hour":"18:29:39","realtime":true},{"line":"56","hour":"18:41:39","realtime":false},{"line":"56","hour":"18:53:39","realtime":false}]
+//let passaggi = [{"line":"56","hour":"17:01:20","realtime":true},{"line":"56","hour":"17:05:31","realtime":true},{"line":"56","hour":"17:19:50","realtime":true},{"line":"56","hour":"17:29:17","realtime":true},{"line":"56","hour":"17:41:57","realtime":true},{"line":"56","hour":"17:57:32","realtime":true},{"line":"56","hour":"18:05:39","realtime":true},{"line":"56","hour":"18:17:39","realtime":true},{"line":"56","hour":"18:29:39","realtime":true},{"line":"56","hour":"18:41:39","realtime":false},{"line":"56","hour":"18:53:39","realtime":false}]
+
+const button = document.getElementsByTagName("button")[0];
+
+button.addEventListener('click', function(){
+
+fetch('https://gpa.madbob.org/query.php?stop=1550')
+  .then(response => response.json())
+  .then(passaggi =>{ console.log(passaggi)});    
+
+});
+
+    
+//
+
+//let nomi = ["Giacomo", "Aldo", "Giovanni", "Marina"];
+let passaggi = [{line:"56",hour:"00:15:51",realtime:"true"}]
+
+
+let prossimaLinea = 56;
+aggiungiOrariListaIniziale([0]);
+
+//
+mostraPassaggio();
+
+function mostraPassaggio(orario , linea){
+
+    
+    let passaggini = "";
+
+    passaggini += <tr>
+    <th class="th-title" id="titoloLinea">Linea</th>
+    <th class="th-title" id="titoloOrario">Orario</th>
+</tr>
+
+    let table = document.getElementById("tabella");
+    let tr = document.createElement("tr");
+    let tdOrari = document.createElement("td");
+    tdOrari.className = "orario";
+    tdOrari.innerHTML = orario;
+    let tdLinea = document.createElement("td");
+    tdLinea.innerHTML = linea;
+    tr.appendChild(tdLinea);
+    tr.appendChild(tdOrari);
+    table.appendChild(tr);
+}
+
+function mostraPassaggio(orario){
+    
+    let table = document.getElementById("tabella");
+    let tr = document.createElement("tr");
+    let tdOrari = document.createElement("td");
+    tdOrari.className = "orario";
+    tdOrari.innerHTML = orario;
+    let tdLinea = document.createElement("td");
+    tdLinea.innerHTML = prossimaLinea;
+    tr.appendChild(tdLinea);
+    tr.appendChild(tdOrari);
+    table.appendChild(tr);
+}
+
+function aggiungiOrariListaIniziale(){
+
+
+    for(let i = 0; i < passaggi.length; i++){
+
+        mostraPassaggio(passaggi[i]);
+//
+        passaggi += "56" + passaggi[i].line + "00:15:51";
+    } 
+}
 
 
 
 
-function mostraPassaggi(linea, orario){
+function mostraPassaggioNuovo(){
+    let orarioNuovo = document.getElementById('orarioNuovo').value;
+   
+    if(orarioNuovo == "" || orarioNuovo == "undefined")
+        alert("Inserisci la fermata");
+    else{
+        mostraPassaggio(orarioNuovo);
+        document.getElementById('orarioNuovo').value = "";
+    }
+
+}
+
+
+
+/*function mostraPassaggi(linea, orario){
 
     let table = document.getElementById('tabella');
     let tr = document.createElement('tr')
@@ -11,17 +95,10 @@ function mostraPassaggi(linea, orario){
     tdLinea.className = "linea";
     tdLinea.innerHTML = linea;
     let tdOrari = document.createElement('td')
-    tdOra.innerHTML = orario;
+    tdOrari.innerHTML = orario;
     tr.appendChild(tdLinea);
     tr.appendChild(tdOrari);
     table.appendChild(tr);
 
-}
+}*/
 
-    
-  
-
-
-/*fetch('https://gpa.madbob.org/query.php?stop=966')
-  .then(response => response.json())
-  .then(passaggi =>{ console.log(passaggi) });*/
---
+++ fetch2.js
@@ -0,0 +1,45 @@
+const button = document.getElementsByTagName("button")[0];
+
+
+
+button.addEventListener('click', function(){
+
+fetch('https://gpa.madbob.org/query.php?stop=27')
+  .then(response => response.json())
+  .then(passaggi =>{ console.log(passaggi)});    
+
+});
+
+
+    let passaggi = [
+        {line:"10",hour:"01:07:23",realtime:false},
+        {line:"49",hour:"01:09:23",realtime:false},
+        {line:"13",hour:"01:17:20",realtime:false},
+        {line:"10",hour:"01:19:23",realtime:false},
+        {line:"10N",hour:"01:40:00",realtime:false},
+        {line:"10N",hour:"01:40:00",realtime:false}];
+
+
+mostraPassaggio();
+
+function mostraPassaggio(){
+
+    let table = document.createElement("table");
+    
+    let innerT = '';
+  
+    innerT += "<tr class='firstRow'><th>Linea</th><th>Orario</th><th>Realtime</th></tr>";
+    
+    for (var i = 0; i < passaggi.length; i++)
+    {
+        innerT += "<tr><td>" + passaggi[i].line + "</td><td>" + passaggi[i].hour + "</td><td>" + passaggi[i].realtime;
+    }
+    
+    table.innerHTML = innerT;
+   
+    document.body.append(table);
+}
+
+
+
+    
