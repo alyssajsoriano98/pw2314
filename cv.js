957fa0e6dad4a5123405afa42c111f2203086cb3
alyssajsoriano98 <130670575+alyssajsoriano98@users.noreply.github.com>
2023-05-22 18:11:45 +0200

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,23 @@
+const exp = document.getElementById('exp');
+
+function mostraExp(){
+
+    if (exp.classList.contains("d-none")){
+        exp.classList.remove("d-none");
+
+
+    } else
+        (exp.classList.contains("exp"))
+        exp.classList.remove("exp");
+        
+}
+
+const istr = document.getElementById('istr');
+
+function mostraIstr(){
+    if (istr.classList.contains("d-none")){
+        exp.classList.remove("d-none");
+
+    }
+}
+
