baf1ab5860083244d110016430a8731b9b5ce97b
alyssajsoriano98 <130670575+alyssajsoriano98@users.noreply.github.com>
2023-05-22 12:57:45 +0200

    Add files via upload

--- fetch2.js
+++ fetch2.js
@@ -1,44 +1,61 @@
-const button = document.getElementsByTagName("button")[0];
+  let passaggi = [
+        {
+            "line":"64","hour":"12:38:01","realtime":true},
+            {"line":"64","hour":"12:53:01","realtime":false},
+            {"line":"64","hour":"13:08:01","realtime":true},
+            {"line":"64","hour":"13:23:01","realtime":false},
+            {"line":"64","hour":"13:38:01","realtime":false},
+            {"line":"64","hour":"13:53:01","realtime":false},
+            {"line":"64","hour":"14:08:01","realtime":false}];
 
+
+         
+
+function mostraPassaggio(line, hour){
+
+    let table = document.createElement("table");
+    
+    let innerT = "";
+
+    let tr = document.createElement("tr");
+    let tdHour = document.createElement("td");
+    tdHour.className = "hour";
+    tdHour.innerHTML = hour;
+    let tdLine = document.createElement("td");
+    tdLine.innerHTML = line;
+    tr.appendChild(tdLine);
+    tr.appendChild(tdHour);
+    table.appendChild(tr);
+
+    
+    innerT += "<tr class='firstRow'><td><b>Linea</b></td><td><b>Orari</b></td><td><b>Linea</b></td></tr>";
+    
+    for (var i = 0; i < passaggi.length; i++)
+    {
+        innerT += "<tr><td>" + passaggi[i].line + "</td><td>" + passaggi[i].hour + "</td><td>" + passaggi[i].realtime + "</td></tr>";
+    }
+    table.innerHTML = innerT;
+    
+    document.body.append(table);
+
+      }
+
+      
+
+
+
+mostraPassaggio();
+
+
+const button = document.getElementsByTagName("button")[0];
 
 
 button.addEventListener('click', function(){
 
-fetch('https://gpa.madbob.org/query.php?stop=27')
+fetch('https://gpa.madbob.org/query.php?stop=1934')
   .then(response => response.json())
   .then(passaggi =>{ console.log(passaggi)});    
 
 });
 
 
-    let passaggi = [
-        {line:"10",hour:"01:07:23",realtime:false},
-        {line:"49",hour:"01:09:23",realtime:false},
-        {line:"13",hour:"01:17:20",realtime:false},
-        {line:"10",hour:"01:19:23",realtime:false},
-        {line:"10N",hour:"01:40:00",realtime:false},
-        {line:"10N",hour:"01:40:00",realtime:false}];
-
-
-mostraPassaggio();
-
-function mostraPassaggio(){
-
-    let table = document.createElement("table");
-    
-    let innerT = "";
-    
-    innerT += "<tr class='firstRow'><th>Linea</th><th>Orario</th><th>Realtime</th></tr>";
-    
-    for (var i = 0; i < passaggi.length; i++)
-    {
-        innerT += "<tr><td>" + passaggi[i].line + "</td><td>" + passaggi[i].hour + "</td><td>" + passaggi[i].realtime;
-    }
-    table.innerHTML = innerT;
-    
-    document.body.append(table);
-}
-
-
-
-    
