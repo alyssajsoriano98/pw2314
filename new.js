f6c4c91af458675e9125d02f213c03abb329468b
alyssajsoriano98 <130670575+alyssajsoriano98@users.noreply.github.com>
2023-06-01 17:16:17 +0200

    Add files via upload

--- new.js
+++ new.js
@@ -4,36 +4,40 @@ let oggetti = [
         data: "1964",
         testobottone: "Fiore",
         colorebottone: "red"
-        
     }
 ];
 
 
-let lista = document.getElementById("lista");
+
 
 function aggiungiOggetti (){
 
 for(let oggetto of oggetti){
+
+    let lista = document.getElementById("lista");
     let card = document.createElement("div");
+    let cardbody = document.createElement("div");
     let titolo = document.createElement("h5");
     let data = document.createElement("p");
     let bottone = document.createElement("button");
-
+    
     titolo.innerHTML = oggetto.titolo;
     data.innerHTML = oggetto.data;
     bottone.innerHTML = oggetto.testobottone;
 
-    card.appendChild(titolo);
-    card.appendChild(data);
-    card.appendChild(bottone);
+    card.appendChild(cardbody);
+    cardbody.appendChild(titolo);
+    cardbody.appendChild(data);
+    cardbody.appendChild(bottone);
     lista.appendChild(card);
 
     card.className = "col-3 card";
+    cardbody.className = "card-body";
     titolo.className = "card-title";
     bottone.className = "btn btn-primary"
+    bottone.style.backgroundColor = oggetto.colorebottone;
 
-   
-
+//    bottone.style.backgroundColor = document.getElementById("colorebottone").value;
 
 }
 }
